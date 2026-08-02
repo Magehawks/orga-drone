bd94e473d3ebd25254b9dfbc5d04041e3eed4ab8
Magehawks <niwes.schult@gmail.com>
2026-08-02 21:46:39 +0200

    Revert "test: intentional failing test to verify CI fails closed"
    This reverts commit 7c50069ea0f519e9086ddeb5cae58352685b3a68.

--- tests/test_ci_intentional_fail.py
+++
@@ -1,4 +0,0 @@
-"""Temporary intentional failure to verify CI fails closed. Do not merge."""
-
-def test_intentional_ci_failure() -> None:
-    assert False, "intentional failure for CI verification"
